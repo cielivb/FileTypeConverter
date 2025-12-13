3c5331d6ecb9a3d5457276e0e012620cdc2de591
Ciel Baumann <ciel.baumann@gmail.com>
2025-12-14 09:17:39 +1300

    Add GUI row 3 and row 4 skeletons

--- converter.py
+++ converter.py
@@ -15,7 +15,7 @@ class Panel(wx.Panel):
         label_source = wx.StaticText(self, label='Source:')
         self.dyn_label_source = wx.StaticText(self, label='')
         self.source_button = wx.Button(self, label='Select')
-        self.source_button.Bind(wx.EVT_BUTTON, self._on_select_source)
+        self.source_button.Bind(wx.EVT_BUTTON, self._on_select)
         r1_sizer.AddMany([(label_source, 0, wx.ALL, 5),
                           (self.dyn_label_source, 0, wx.ALL, 5),
                           (self.source_button, 0, wx.ALL, 5)])
@@ -34,10 +34,24 @@ class Panel(wx.Panel):
         
         # Row 3 : 'Output location:', dynamic dir label, change button
         r3_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        label_out = wx.StaticText(self, label='Output location:')
+        self.dyn_label_out = wx.StaticText(self, label='')
+        self.change_button = wx.Button(self, label='Change')
+        self.change_button.Bind(wx.EVT_BUTTON, self._on_change)
+        r3_sizer.AddMany([(label_out, 0, wx.ALL, 5),
+                          (self.dyn_label_out, 0, wx.ALL, 5),
+                          (self.change_button, 0, wx.ALL, 5)])        
         
         # Row 4 : convert button, open output location button
         r4_sizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.convert_button = wx.Button(self, label='Convert')
+        self.open_button = wx.Button(self, label='Open output location')
+        self.convert_button.Bind(wx.EVT_BUTTON, self._on_convert)
+        self.open_button.Bind(wx.EVT_BUTTON, self._on_open)
+        r4_sizer.AddMany([(self.convert_button, 0, wx.ALL, 5),
+                          (self.open_button, 0, wx.ALL, 5)])
         
+        # Top level sizing
         main_sizer.AddMany([(r1_sizer, 0, wx.ALIGN_CENTRE, 5),
                             (r2_sizer, 0, wx.ALIGN_CENTRE, 5),
                             (r3_sizer, 0, wx.ALIGN_CENTRE, 5),
@@ -45,8 +59,17 @@ class Panel(wx.Panel):
         self.SetSizer(main_sizer)
     
     
-    def _on_select_source(self, event):
+    def _on_select(self, event):
         print('select source button pressed')
+    
+    def _on_change(self, event):
+        print('change button pressed')
+    
+    def _on_convert(self, event):
+        print('on convert button pressed')
+    
+    def _on_open(self, event):
+        print('open output location button pressed')
 
 
 class Frame(wx.Frame):
